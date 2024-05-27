475e6a498d04e1ec9ce0f60931f6ee56ce9b2b50
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-05-27 14:24:15 -0300

    fix: add a related_name to owner field for easy referencing

--- todo/models.py
+++ todo/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 
-class TodoModel(models.Model):
+class Todo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     text = models.CharField(max_length=1000)
     completed = models.BooleanField(default=False)
-    owner = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        "auth.User", related_name="todos", on_delete=models.CASCADE
+    )
 
     class Meta:
         ordering = ["created_at"]
