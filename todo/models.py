d3a7b3718723aeaa8d417e7b2d909ff9de5e7b0c
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-05-27 14:08:10 -0300

    feat: add todo model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class TodoModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.CharField(max_length=1000)
+    completed = models.BooleanField(default=False)
+    owner = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["created_at"]
