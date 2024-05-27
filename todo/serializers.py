a7ee02e01c13e46ed7c8cefb3e648e8229285007
Mateus Colombo Godoy <mateuscgodoy@gmail.com>
2024-05-27 14:25:46 -0300

    feat: add TodoSerializer and UserSerializer to serializers.py

---
+++ todo/serializers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Todo
+
+
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source="owner.username")
+
+    class Meta:
+        model = Todo
+        fields = ["url", "id", "text", "completed"]
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    todos = serializers.HyperlinkedIdentityField(
+        many=True, view_name="todo-detail", read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = ["url", "id", "username", "todos"]
